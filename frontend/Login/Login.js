4c21d5f321d3b25d5bd54b06cfd856973ee402ba
M-Abdullah-Azeem <m.abdullah.azeem.official@gmail.com>
2025-04-13 18:12:00 +0500

    Implement Donor Login Page Connectivity.

--- frontend/Login/Login.js
+++ frontend/Login/Login.js
@@ -1,8 +1,34 @@
+loginForm = document.querySelector(".login-form");
+
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("../footer.html")
-      .then(res => res.text())
-      .then(data => {
-        document.getElementById("footer-container").innerHTML = data;
-      });
-  });
-  
+  fetch("../footer.html")
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("footer-container").innerHTML = data;
+    });
+});
+
+loginForm.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  try {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch(`${BASE_URL}/api/donors/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ email, password }),
+    });
+    if (res.ok) {
+      alert("Login Successful");
+    } else {
+      alert("User with given Email and Password does not exist.");
+    }
+  } catch (e) {
+    console.log(e);
+    alert("Oops, Something went wrong. Try Again Later.");
+  }
+});
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import routes from './routes/index.routes';
-import AppDataSource from './config/ormConfig';
-import http from 'http';
-import dotenv from 'dotenv';
+import cors from "cors";
+import express from "express";
+import routes from "./routes/index.routes";
+import AppDataSource from "./config/ormConfig";
+import http from "http";
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
@@ -11,14 +12,19 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    credentials: true,
+  })
+);
 // Import and use routes
-app.use('/api', routes);
+app.use("/api", routes);
 
-AppDataSource.initialize().then(async() => {
-// Start the server
-    const server = http.createServer(app);
-    server.listen(PORT, () => {
+AppDataSource.initialize().then(async () => {
+  // Start the server
+  const server = http.createServer(app);
+  server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    });
-
+  });
 });
