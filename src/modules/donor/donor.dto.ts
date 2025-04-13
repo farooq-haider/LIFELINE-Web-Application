66b1468ec90c84d723ad22c1d5341584bd019e43
basimzafar <basim.zaf@gmail.com>
2025-04-13 20:01:56 +0500

    Updated About Us page layout and icon positioning

--- src/modules/donor/donor.dto.ts
+++ src/modules/donor/donor.dto.ts
@@ -1,48 +1,66 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export enum BloodGroup {
-    A_POSITIVE = 'A+',
-    A_NEGATIVE = 'A-',
-    B_POSITIVE = 'B+',
-    B_NEGATIVE = 'B-',
-    AB_POSITIVE = 'AB+',
-    AB_NEGATIVE = 'AB-',
-    O_POSITIVE = 'O+',
-    O_NEGATIVE = 'O-',
+  A_POSITIVE = "A+",
+  A_NEGATIVE = "A-",
+  B_POSITIVE = "B+",
+  B_NEGATIVE = "B-",
+  AB_POSITIVE = "AB+",
+  AB_NEGATIVE = "AB-",
+  O_POSITIVE = "O+",
+  O_NEGATIVE = "O-",
 }
 
 export const CreateDonorDTOSchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    password: z.string().min(8, 'Password must be at least 8 characters long'),
-    bloodGroup: z.nativeEnum(BloodGroup),
-    lastDonation: z.string(),
-    phone: z.string(),
-    address: z.string(),
-    city: z.string(),
-    isActive: z.boolean().default(true),
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
+  bloodGroup: z.nativeEnum(BloodGroup),
+  lastDonation: z.string(),
+  phone: z.string(),
+  address: z.string(),
+  city: z.string(),
+  isActive: z.boolean().default(true),
 });
 
+export const UpdateDonorDTOSchema = z.object({
+  name: z.string().optional(),
+  email: z.string().email().optional(),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .optional(),
+  bloodGroup: z.nativeEnum(BloodGroup).optional(),
+  lastDonation: z.string().optional(),
+  phone: z.string().optional(),
+  address: z.string().optional(),
+  city: z.string().optional(),
+  isActive: z.boolean().default(true).optional(),
+});
 
 export const DonorResponseDTOSchema = z.object({
-    id: z.number().int().positive(),
-    name: z.string(),
-    email: z.string().email(),
-    bloodGroup: z.nativeEnum(BloodGroup),
-    lastDonation: z.string(),
-    phone: z.string(),
-    address: z.string(),
-    city: z.string(),
-    isActive: z.boolean(),
-    createdAt: z.date(),
-    updatedAt: z.date(),
+  id: z.number().int().positive(),
+  name: z.string(),
+  email: z.string().email(),
+  bloodGroup: z.nativeEnum(BloodGroup),
+  lastDonation: z.string(),
+  phone: z.string(),
+  address: z.string(),
+  city: z.string(),
+  isActive: z.boolean(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
 });
 
 export const LoginDonorDTOSchema = z.object({
-    email: z.string().email('Invalid email address').transform((val) => val.toLowerCase()),
-    password: z.string()
+  email: z
+    .string()
+    .email("Invalid email address")
+    .transform((val) => val.toLowerCase()),
+  password: z.string(),
 });
 
 export type CreateDonorDTO = z.infer<typeof CreateDonorDTOSchema>;
 export type DonorResponseDTO = z.infer<typeof DonorResponseDTOSchema>;
 export type LoginDonorDTO = z.infer<typeof LoginDonorDTOSchema>;
+export type UpdateDonorDTO = z.infer<typeof UpdateDonorDTOSchema>;
--- src/modules/donor/donor.service.ts
+++ src/modules/donor/donor.service.ts
@@ -1,73 +1,120 @@
-import bcrypt from 'bcrypt';
-import DonorRepository from './donor.repository';
-import Donor from './donor.entity';
-import { CreateDonorDTO, LoginDonorDTO } from './donor.dto';
-import jwt from 'jsonwebtoken';
-import config from '../../config/config';
+import bcrypt from "bcrypt";
+import DonorRepository from "./donor.repository";
+import Donor from "./donor.entity";
+import {
+  CreateDonorDTO,
+  DonorResponseDTO,
+  LoginDonorDTO,
+  UpdateDonorDTO,
+} from "./donor.dto";
+import jwt from "jsonwebtoken";
+import config from "../../config/config";
 
-export default class DonorService {
-    static async getAllDonors(): Promise<Donor[]> {
-        const donors = await DonorRepository.findAll();
-        return donors;
+export enum ValidBloodGroup {
+  APositive = "A+",
+  ANegative = "A-",
+  BPositive = "B+",
+  BNegative = "B-",
+  ABPositive = "AB+",
+  ABNegative = "AB-",
+  OPositive = "O+",
+  ONegative = "O-",
+}
+
+export class DonorService {
+  static async getAllDonors(): Promise<DonorResponseDTO[]> {
+    const donors = await DonorRepository.findAll();
+    return donors;
+  }
+
+  static async getDonorById(id: number): Promise<DonorResponseDTO> {
+    const donor = await DonorRepository.findById(id);
+    if (!donor) {
+      throw new Error("Donor not found");
+    }
+    return donor;
+  }
+
+  static async getDonorByEmail(email: string): Promise<Donor> {
+    const donor = await DonorRepository.findByEmail(email);
+    if (!donor) {
+      throw new Error("Donor not found");
+    }
+    return donor;
+  }
+
+  static async getDonorsByLocation(
+    city: string,
+    bloodGroup: ValidBloodGroup
+  ): Promise<DonorResponseDTO[]> {
+    const donors = await DonorRepository.findByLocation(bloodGroup, city);
+    if (!donors || donors.length === 0) {
+      throw new Error("No donors found in this location");
+    }
+    return donors;
+  }
+
+  static async createDonor(data: CreateDonorDTO): Promise<string> {
+    const donor = await DonorRepository.findByEmail(data.email);
+    if (donor) {
+      throw new Error("Donor with the email already exists");
     }
 
-    static async getDonorById(id: number): Promise<Donor> {
-        const donor = await DonorRepository.findById(id);
-        if (!donor) {
-            throw new Error('Donor not found');
-        }
-        return donor;
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    data.password = hashedPassword;
+    const newDonor = await DonorRepository.create(data);
+    const userSecret = jwt.sign(
+      { donorId: newDonor.id, donorEmail: newDonor.email },
+      config.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    return userSecret;
+  }
+
+  static async loginDonor(data: LoginDonorDTO): Promise<string> {
+    const donor = await DonorRepository.findByEmail(data.email);
+    if (!donor) {
+      throw new Error("Invalid email or password");
     }
 
-    static async getDonorByEmail(email: string): Promise<Donor> {
-        const donor = await DonorRepository.findByEmail(email);
-        if (!donor) {
-            throw new Error('Donor not found');
-        }
-        return donor;
+    const isPasswordValid = await bcrypt.compare(data.password, donor.password);
+    if (!isPasswordValid) {
+      throw new Error("Invalid email or password");
     }
 
-    static async createDonor(data: CreateDonorDTO): Promise<string> {
-        const donor = await DonorRepository.findByEmail(data.email);
-        if (donor) {
-            throw new Error('Donor with the email already exists');
-        }
+    const userSecret = jwt.sign(
+      { donorId: donor.id, donorEmail: donor.email },
+      config.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
 
-        const hashedPassword = await bcrypt.hash(data.password, 10);
-        data.password = hashedPassword;
-        const newDonor = await DonorRepository.create(data);
-        const userSecret = jwt.sign({ donorId: newDonor.id, donorEmail: newDonor.email }, config.JWT_SECRET, { expiresIn: '1h' });
+    return userSecret;
+  }
 
-        return userSecret;
+  static async updateDonor(
+    id: number,
+    data: UpdateDonorDTO
+  ): Promise<DonorResponseDTO> {
+    const donor = await DonorRepository.findById(id);
+    if (!donor) {
+      throw new Error("Donor not found");
+    }
+    const result = DonorRepository.update(id, data);
+    return result;
+  }
+
+  static async deleteDonor(id: number): Promise<void> {
+    const donor = await DonorRepository.findById(id);
+    if (!donor) {
+      throw new Error("Donor not found");
     }
 
-    static async loginDonor(data: LoginDonorDTO): Promise<string> {
-        const donor = await DonorRepository.findByEmail(data.email);
-        if (!donor) {
-            throw new Error('Invalid email or password');
-        }
-
-        const isPasswordValid = await bcrypt.compare(data.password, donor.password);
-        if (!isPasswordValid) {
-            throw new Error('Invalid email or password');
-        }
-
-        const userSecret = jwt.sign({ donorId: donor.id, donorEmail: donor.email }, config.JWT_SECRET, { expiresIn: '1h' });
-
-        return userSecret;
+    const result = await DonorRepository.deleteById(id);
+    if (!result) {
+      throw new Error("Failed to delete donor");
     }
 
-    static async deleteDonor(id: number): Promise<void> {
-        const donor = await DonorRepository.findById(id);
-        if (!donor) {
-            throw new Error('Donor not found');
-        }
-
-        const result = await DonorRepository.deleteById(id);
-        if (!result) {
-            throw new Error('Failed to delete donor');
-        }
-
-        return;
-    }
+    return;
+  }
 }
